18ec433d3a38e50ae1a7e156727299ff71d5c5dc
Enes Alp <enesalp1313@gmail.com>
2020-06-20 00:50:30 +0300

    first commit on new branch

--- test.py
+++ test.py
@@ -5,8 +5,11 @@ class ModelUydu:
 		P = 0;
 	def set_Preasure(self,P):
 		self.P = P
+	def get_Preasure(self):
+		return self.P
 
-Model1 = ModelUydu();
-Model1.set_Preasure(48.78);
+Model1 = ModelUydu()
+Model1.set_Preasure(48.78)
 print("P = ",Model1.P)
+print("P = ", Model1.get_Preasure())
 
